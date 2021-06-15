f17212efd549ae09f1edd6890ccfbffb9d34edee
zen <zen@zen>
2021-06-15 08:41:30 -0700

    starting with gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/tech-pool/takemetotheshop-api/api/registration"
 )
 
@@ -11,7 +11,25 @@ const address string = ":8080"
 
 func main() {
 
-	http.HandleFunc("/registration", registration.Handler)
+	router := setupRouter()
+	router.Run(address)
 
-	log.Fatalln(http.ListenAndServe(address, nil))
+	//http.HandleFunc("/registration", registration.Handler)
+	// log.Fatalln(http.ListenAndServe(address, nil))
+}
+
+func setupRouter() *gin.Engine {
+
+	///Engine  returned by Default() is the framework's instance,
+	//it contains the muxer, middleware and configuration settings.
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello Gin")
+	})
+
+	r.GET("/registration", registration.Handler)
+
+	return r
+
 }
