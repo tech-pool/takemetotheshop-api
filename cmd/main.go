dd979e82f917733f21747f3136cdbfeb9c9ef4f5
zen <zen@zen>
2021-06-16 09:57:34 -0700

    add configuration file

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tech-pool/takemetotheshop-api/api/registration"
+	"gopkg.in/yaml.v2"
 )
 
-// to be put in a config file
-const default_address string = ":8080"
+type yamlCfg struct {
+	Port string `yaml:"default_port"`
+}
 
 var port string
 
@@ -17,7 +21,21 @@ func init() {
 	port = os.Getenv("TECHSHOP_PORT")
 
 	if port == "" {
-		port = default_address
+		cfg := yamlCfg{}
+
+		yamlFile, err := ioutil.ReadFile("../configs/.config.yaml")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = yaml.Unmarshal(yamlFile, &cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		port = cfg.Port
+
 	}
 }
 
